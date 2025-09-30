b45fd9d50a72305e495a98ab54200837fc27bd23
Naaidrv <zabriverav@gmail.com>
2025-09-29 21:46:14 -0600

    Fix: Song - Fuhr mich nochmal in den Sturm

--- js/music-player.js
+++ js/music-player.js
@@ -19,7 +19,7 @@
             ],
             leidenschaft: [
                 { id: 1, name: "Liebe über Leben", artist: "Lacrimosa", album: "Leidenschaft", duration: "6:12", durationSeconds: 415, artwork: "/img/leidenschaft.jpeg", audioUrl: "/songs/Leidenschaft/Liebe uber Leben.mp3" },
-                { id: 2, name: "Führ mich nochmal in den Sturm", artist: "Lacrimosa", album: "Leidenschaft", duration: "6:00", durationSeconds: 344, artwork: "/img/leidenschaft.jpeg", audioUrl: "/songs/Leidenschaft/Führ mich nochmal in den Sturm.mp3" },
+                { id: 2, name: "Führ mich nochmal in den Sturm", artist: "Lacrimosa", album: "Leidenschaft", duration: "6:00", durationSeconds: 344, artwork: "/img/leidenschaft.jpeg", audioUrl: "/songs/Leidenschaft/Fuhr mich nochmal in den Sturm.mp3" },
                 { id: 3, name: "Kulturasche", artist: "Lacrimosa", album: "Leidenschaft", duration: "4:22", durationSeconds: 344, artwork: "/img/leidenschaft.jpeg", audioUrl: "/songs/Leidenschaft/Kulturasche.mp3" },
                 { id: 4, name: "The Daughter Of Coldness", artist: "Lacrimosa", album: "Leidenschaft", duration: "4:32", durationSeconds: 344, artwork: "/img/leidenschaft.jpeg", audioUrl: "/songs/Leidenschaft/The Daughter Of Coldness.mp3" },
                 { id: 5, name: "Raubtier", artist: "Lacrimosa", album: "Leidenschaft", duration: "4:57", durationSeconds: 344, artwork: "/img/leidenschaft.jpeg", audioUrl: "/songs/Leidenschaft/Raubtier.mp3" },
